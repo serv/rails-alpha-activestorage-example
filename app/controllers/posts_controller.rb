d09e189bd31b1982140b2748c393be772815e5a2
Jason Kim <iamjsonkim@gmail.com>
2017-11-13 23:40:07 -0800

    mvc completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  # we will display post form here
+  def index
+    @post = Post.new
+  end
+  # we will create post here
+  def create
+    post = Post.create! params.require(:post).permit(:title, :body)
+    post.images.attach(params[:post][:images])
+    redirect_to post
+  end
+  # we will display post with photo
+  def show
+    @post = Post.find(params[:id])
+  end
 end
